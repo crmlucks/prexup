3566afcf0672d4fdc97317155f0262e9669caa06
Admin <admin@prexcrm.com>
2026-05-03 22:25:45 -0500

    fix: limitar memoria de nextjs para evitar errores en vps

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+    memoryBasedWorkersCount: true
   }
 };
 
